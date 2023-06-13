697019f6f27c349bb1825d61d9f4d4d6695b124b
tomascastrop <tomascastropincheira@gmail.com>
2023-06-13 03:30:55 +0000

    codigo4

---
+++ tomas4.py
@@ -0,0 +1,17 @@
+import requests
+import netmiko
+import json
+from netmiko import ConnectHandler
+
+cisco1 = { "ip": "192.168.184.130",
+"device_type": "cisco_ios",
+"username": "cisco",
+"password": "cisco123!",
+}
+command = "show version"
+with ConnectHandler(**cisco1) as net_connect:
+    output = net_connect.send_command(command)
+    print()
+    print(output)
+    print()
+i
